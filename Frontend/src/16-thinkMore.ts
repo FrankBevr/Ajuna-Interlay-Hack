d312a00ea939087aa41d3e881b2526a48daf3670
FrankBevr <frank_dierolf@web.de>
2023-06-29 10:39:44 +0200

    :zap: (./frontend) add chapter 16

---
+++ Frontend/src/16-thinkMore.ts
@@ -0,0 +1,10 @@
+export function chapter_thinkMore(element: HTMLDivElement) {
+  element.innerHTML = `
+    <h3>Think more</h3>
+    Ok now we know some stuff.
+    So the question remains.
+    How do I make a button, which allows you to buy an NFT and you get another Asset for free on another chain.
+    XCM feels like a promising way to achieve it.
+    But it could be that I need chain extension in the contract to even make it work and bring the whole logic into the digital asset itself. 
+  `
+}
