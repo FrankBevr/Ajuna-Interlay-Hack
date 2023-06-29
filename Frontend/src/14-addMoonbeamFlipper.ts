d5a1ce895d27322c60f5ec0d250df04ea111aabd
FrankBevr <frank_dierolf@web.de>
2023-06-29 10:39:09 +0200

    :zap: (./frontend) add chapter 14

---
+++ Frontend/src/14-addMoonbeamFlipper.ts
@@ -0,0 +1,9 @@
+export function chapter_addMoonbeamFlipper(element: HTMLDivElement) {
+  element.innerHTML = `
+    <h3>Add Moonbeam and create Flipper Contract</h3>
+    1. Write an Flipper
+    2. Deploy an Flipper
+    3. Call Smart Contract
+    4. Call Smart contract in frontend
+  `;
+}
