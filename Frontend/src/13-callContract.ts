02600e5e9d6e5d4efa425b584d3a719b018e312e
FrankBevr <frank_dierolf@web.de>
2023-06-29 10:38:55 +0200

    :zap: (./frontend) add chapter 13

---
+++ Frontend/src/13-callContract.ts
@@ -0,0 +1,11 @@
+export function chapter_callContract(element: HTMLDivElement) {
+  element.innerHTML = `
+    <h3>Call Contract</h3>
+    1. Go to PolkadotJsApp 
+    2. Go to the extrinsic
+    3. Use the xcm pallet and call the contract
+    4. Reproduce it via the polkadotJSAPi
+    5. Make a component which show the output of the call
+    6. Include it in the main.ts file.
+  `;
+}
