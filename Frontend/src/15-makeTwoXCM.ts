b2fe0da2f12ea94b541f6e617c18a74b17a5d4d8
FrankBevr <frank_dierolf@web.de>
2023-06-29 10:39:33 +0200

    :zap: (./frontend) add chapter 15

---
+++ Frontend/src/15-makeTwoXCM.ts
@@ -0,0 +1,13 @@
+export function chapter_makeTwoXCM(element: HTMLDivElement) {
+  element.innerHTML = `
+    <h3>Make two XCM Transaction</h3>
+    1. Go to PolkadotJsApp 
+    2. Go to the extrinsic
+    3. Use the xcm pallet and call the NFT contract 
+    4. Use the xcm pallet and call the Flipper contract
+    5. Reproduce it via the polkadotJSAPi
+    5. Make a component which shows the output of the call
+    6. Make it clickalbe. If click, show output.
+    7. Include it in the main.ts file.
+  `
+}
